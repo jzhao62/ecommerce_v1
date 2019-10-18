3a910110e8d4096922d31f7b600de73df7737280
jingyi <zhao.elton@gmail.com>
2019-10-18 06:06:44 -0400

    added skyrich project description, more cleanups

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React, { Component } from 'react';
-
-
 import {Card}from './models/card.js'
-import {MyCoordinateMarker, MyMapComponent} from "./models/Marker/MapMarker";
 
 
 
 
+const  App = () => {
+    return (
+        <div>
+            <Card />
+        </div>
+    )
 
-
-class App extends Component {
-
-    state = {
-        color: 'skyblue',
-        value: ' ',
-        start_location : ' ',
-        end_location : ' '
-    }
-
-
-
-
-
-
-    render() {
-
-
-        const style = {
-            padding: 40,
-        }
-
-
-
-        return (
-            <div style={style}>
-
-                <Card />
-
-
-            </div>
-        )
-    }
 }
 
 
