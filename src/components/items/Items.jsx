137b9b5ae287ee43a0c8f71a0c690745f0b690fd
jingyi <zhao.elton@gmail.com>
2019-10-31 04:25:10 -0400

    add css for items

--- src/components/items/Items.jsx
+++ src/components/items/Items.jsx
@@ -1,6 +1,30 @@
+import React from "react";
+import "./item.css"
+
+const items = [
+    {name: 'item1'},
+    {name: 'item2'},
+    {name: 'item3'},
+    {name: 'item4'},
+    {name: 'item5'},
+    {name: 'item6'},
+    {name: 'item7'},
+    {name: 'item8'},
+    {name: 'item9'},
+    {name: 'item10'},
+    {name: 'item11'},
+
+]
 
 
 
 export const Items = () => {
-    return <div> </div>
+    return (
+        <div className='container'>
+            {items.map(el => (<div className='card'> {el.name}</div>))}
+        </div>
+    )
+
+
+
 }
